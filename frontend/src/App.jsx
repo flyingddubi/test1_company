f40784d790f9b76264c7c42aa1366bb5eb599090
flyingddubi <rlaendus77@gmail.com>
2025-12-03 11:17:23 +0900

    ch1-5. 하단 바

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
