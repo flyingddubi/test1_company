087a9a91a26b6af203d291b5852f5abdcfe2b826
flyingddubi <rlaendus77@gmail.com>
2025-12-10 11:19:05 +0900

    DB정보 셋팅1

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2/promise");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+// MariaDB 연결
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || null,
+});
+
+connection
+  .then((conn) => {
+    console.log("MariaDB 접근 성공");
+    // 연결을 전역으로 저장하여 나중에 사용할 수 있도록 함
+    app.locals.db = conn;
+  })
+  .catch((error) => {
+    console.log("MariaDB 접근 실패", error);
+  });
+
 app.listen(port, () => {
-  console.log("Server is running at http://localhost:${port}");
+  console.log(`Server is running at http://localhost:${port}`);
 });
