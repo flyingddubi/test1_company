2f33f43c89cd3372df250e8ebfc4ccbbc147f3b1
flyingddubi <rlaendus77@gmail.com>
2025-12-16 13:15:05 +0900

    ch5-4.인증및관리자초기화면

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,14 @@ import "./App.css";
 
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import AdminNavbar from "./components/AdminNavbar/AdminNavbar";
 
-import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import axios from "axios";
 
 import MainPage from "./page/MainPage/MainPage";
@@ -15,14 +21,22 @@ import Contact from "./page/Contact/Contact";
 
 import AdminLogin from "./page/Admin/AdminLogin";
 import { useEffect, useState } from "react";
+import AdminCreatepost from "./page/Admin/AdminCreatepost";
+import AdminEditPost from "./page/Admin/AdminEditPost";
+import AdminContacts from "./page/Admin/AdminContacts";
+import AdminPosts from "./page/Admin/AdminPosts";
 
 function AuthRedirectRoute() {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
 
   useEffect(() => {
     const verifyToken = async () => {
-      try{
-        const response = await axios.post("http://localhost:3000/api/auth/verify-token", {}, { withCredentials: true });
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/verify-token",
+          {},
+          { withCredentials: true }
+        );
         setIsAuthenticated(true);
       } catch (error) {
         console.log("토큰 인증 실패: " + error);
@@ -32,11 +46,45 @@ function AuthRedirectRoute() {
     verifyToken();
   }, []);
 
-  if(isAuthenticated === null){
+  if (isAuthenticated === null) {
     return null;
   }
 
   return isAuthenticated ? <Navigate to="/admin/posts" replace /> : <Outlet />;
+}
+
+function ProtectedRoute() {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/verify-token",
+          {},
+          { withCredentials: true }
+        );
+        setIsAuthenticated(response.data.isValid);
+        setUser(response.data.user);
+      } catch (error) {
+        console.log("토큰 인증 실패: " + error);
+        setIsAuthenticated(false);
+        setUser(null);
+      }
+    };
+    verifyToken();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? (
+    <Outlet context={{ user }} />
+  ) : (
+    <Navigate to="/admin" replace />
+  )
 }
 
 function Layout() {
@@ -49,43 +97,79 @@ function Layout() {
   );
 }
 
+function AdminLayout() {
+  return (
+    <>
+      <AdminNavbar />
+      <Outlet />
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children:[
+    children: [
       {
         index: true,
-        element: <MainPage />
+        element: <MainPage />,
       },
       {
         path: "about",
-        element: <About />
+        element: <About />,
       },
       {
         path: "leadership",
-        element: <Leadership />
+        element: <Leadership />,
       },
       {
         path: "board",
-        element: <Board />
+        element: <Board />,
       },
       {
         path: "our-services",
-        element: <Services />
+        element: <Services />,
       },
       {
         path: "contact",
-        element: <Contact />
-      }
-    ]
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "/admin",
     element: <AuthRedirectRoute />,
-    children: [{ index: true, element: <AdminLogin />}],
+    children: [{ index: true, element: <AdminLogin /> }],
   },
-])
+  {
+    path: "/admin",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        element: <AdminLayout />,
+        children: [
+          {
+            path: "posts",
+            element: <AdminPosts />,
+          },
+          {
+            path: "create-post",
+            element: <AdminCreatepost />,
+          },
+          {
+            path: "edit-post/:id",
+            element: <AdminEditPost />,
+          },
+          {
+            path: "contacts",
+            element: <AdminContacts />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ frontend/src/page/Admin/AdminCreatepost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminCreatepost = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminCreatepost
